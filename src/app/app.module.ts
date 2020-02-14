ce79b22002d1cacec083313c3f6b981317af23c9
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-02-14 16:47:18 +0530

    Template driven forms basic done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormsComponent
+    ReactiveFormsComponent,
+    TemplateDrivenFormsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-driven-forms/template-driven-forms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-template-driven-forms',
+  templateUrl: './template-driven-forms.component.html',
+  styleUrls: ['./template-driven-forms.component.scss']
+})
+export class TemplateDrivenFormsComponent implements OnInit {
+
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  get diagnostic() { return JSON.stringify(this.model); }
+
+}
